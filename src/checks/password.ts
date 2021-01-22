1f5a1d0dfec2c1711cf3e4de8ab923a8f8a27ce2
Henrique Leite <henriqueleite616@gmail.com>
2021-01-22 07:49:56 -0300

    Add Length Limit To Password Validation And Remove Package From GitHub Registry

--- src/checks/password.ts
+++ src/checks/password.ts
@@ -1,6 +1,6 @@
 import { makeFunction } from "checks/helpers";
 
-const IS_STRONG_PASSWORD = /^.*(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).*$/;
+const IS_STRONG_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@$!%*#?&]{6,24}$/;
 export const isStrongPassword = makeFunction({
 	regex: IS_STRONG_PASSWORD,
 });
