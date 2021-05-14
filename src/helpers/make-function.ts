c84968a9dddc9e042797eec3b42ccddaccdb1428
Henrique Leite <henriqueleite616@gmail.com>
2021-05-14 14:22:35 -0300

    Fix isLeap

--- src/helpers/make-function.ts
+++ src/helpers/make-function.ts
@@ -3,18 +3,20 @@ interface IParams<T> {
 	func?: (str: T) => boolean;
 }
 
-export const makeFunction = <T>({ regex, func }: IParams<T>) => (value: T) => {
-	try {
-		if (regex && typeof value === "string") {
-			return regex.test(value);
-		}
+export const makeFunction =
+	<T>({ regex, func }: IParams<T>) =>
+	(value: T) => {
+		try {
+			if (regex && typeof value === "string") {
+				return regex.test(value);
+			}
 
-		if (func) {
-			return func(value);
-		}
+			if (func) {
+				return func(value);
+			}
 
-		return false;
-	} catch (e) {
-		return false;
-	}
-};
+			return false;
+		} catch (e) {
+			return false;
+		}
+	};
